8a061cc3354359dc370eae1b77ffe6e4b4b1405e
Alexey Bobrikov <a.beaverg@gmail.com>
2024-05-09 11:54:44 +0300

    Compared basket tests to 1 test using data provider

--- src/test/java/com/beaverg/tests/BasketTest.java
+++ src/test/java/com/beaverg/tests/BasketTest.java
@@ -1,16 +1,15 @@
 package com.beaverg.tests;
 
 import com.beaverg.base.BaseTest;
-import com.beaverg.components.drop.BasketDropComponent;
 import com.beaverg.domain.ProductCard;
 import com.beaverg.pages.BasketPage;
 import com.beaverg.pages.HomePage;
 import com.beaverg.pages.ProductListPage;
-import com.beaverg.utils.PropertyGetter;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -21,46 +20,10 @@ import java.util.List;
 @Feature("Basket testing")
 public class BasketTest extends BaseTest {
 
-    @Test
+    @Test(dataProvider = "productsNumber")
     @Story("Adding products to basket testing")
-    @Description("Verifying Adding 1 product to basket test")
-    public void verifyAddingOneProductToBasketTest() {
-        SoftAssert sa = new SoftAssert();
-        HomePage homePage = getHomePage();
-
-        ProductListPage mobilePhonesAndroidPage = homePage
-                .getMainMenuComponent()
-                .clickMobilePhonesItem()
-                .clickAndroidIcon();
-        sa.assertTrue(mobilePhonesAndroidPage.isPageOpen("Android OS"),
-                "Android mobile phones Page isn't open!");
-
-        int productIndex = Integer.parseInt(PropertyGetter.getData("product_index"));
-        ProductCard productCardFromProductsPage = mobilePhonesAndroidPage
-                .getProductList()
-                .getProductCardByIndex(productIndex);
-        BasketDropComponent basketDropComponent = mobilePhonesAndroidPage
-                .getProductList()
-                .clickAddToBasketByIndex(productIndex);
-        BasketPage basketPage = basketDropComponent
-                .clickGoToCheckoutButton();
-        sa.assertTrue(basketPage.isPageOpen("My Shopping Basket"),
-                "Basket Page isn't open!");
-        sa.assertEquals(basketPage.getBasketItemTitles().size(), 1,
-                "Number of products in basket isn't equal to quantity added!");
-
-        ProductCard productCardFromBasket = basketPage
-                .getBasketItemByIndex(0);
-        sa.assertEquals(productCardFromBasket, productCardFromProductsPage,
-                "Product Cards aren't equal!");
-
-        sa.assertAll();
-    }
-
-    @Test
-    @Story("Adding products to basket testing")
-    @Description("Verifying Adding 2 products to basket test")
-    public void verifyAddingTwoProductsToBasketTest() {
+    @Description("Verifying Adding 1/2/3 products to basket test")
+    public void verifyAddingSomeProductsToBasketTest(int productsNumber) {
         SoftAssert sa = new SoftAssert();
         HomePage homePage = getHomePage();
 
@@ -72,7 +35,7 @@ public class BasketTest extends BaseTest {
                 "Android mobile phones Page isn't open!");
 
         List<ProductCard> cardsFromProductsPage = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < productsNumber; i++) {
             cardsFromProductsPage.add(mobilePhonesAndroidPage.getProductList().getProductCardByIndex(i));
             mobilePhonesAndroidPage.getProductList().clickAddToBasketByIndex(i);
             mobilePhonesAndroidPage.refreshCurrentPage();
@@ -84,13 +47,20 @@ public class BasketTest extends BaseTest {
         sa.assertTrue(basketPage.isPageOpen("My Shopping Basket"), "Basket Page isn't open!");
 
         List<ProductCard> cardsFromBasket = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < productsNumber; i++) {
             cardsFromBasket.add(basketPage.getBasketItemByIndex(i));
         }
         sa.assertEquals(cardsFromBasket, cardsFromProductsPage, "Product Cards aren't equal!");
-        sa.assertEquals(basketPage.getBasketItemTitles().size(), 2,
+        sa.assertEquals(basketPage.getBasketItemTitles().size(), productsNumber,
                 "Number of products in basket isn't equal to quantity added!");
 
         sa.assertAll();
+    }
+
+    @DataProvider(name = "productsNumber")
+    public Object[][] getProductsNumber() {
+        return new Object[][]{
+                { 1 }, { 2 }, { 3 }
+        };
     }
 }
